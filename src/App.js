7e4a4ac1bb61de27738cfd56329215fb23e7c78c
ck2e14 <chriswkennedy@icloud.com>
2019-11-05 12:31:00 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            peopleInSpace: []
+        }
+    }
+
+    render(){
+        return(
+        <div>
+            {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+        </div>
+        )}
+
+
+        
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(astros => this.setState({
+            peopleInSpace: astros.people
+        }))
+    }
+
+}
